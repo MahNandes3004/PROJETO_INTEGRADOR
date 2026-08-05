d3830443febb95c246fcf9e084d9301831e4945a
Maiara C. Fernandes Da Silva <maiara.fernandes.silva@escola.pr.gov.br>
2026-08-05 09:02:14 -0300

    s

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-// 1. Função para Alto Contraste
-function alternarAltoContraste() {
-    // A função 'toggle' liga a classe se ela não existir, e desliga se ela já existir
-    document.body.classList.toggle('alto-contraste');
+let tamanho = 20;
+
+function aumentarFonte(){
+
+tamanho+=2;
+
+document.body.style.fontSize=tamanho+"px";
+
 }
 
-// 2. Função para Aumentar/Diminuir a Fonte Dinamicamente
-let tamanhoAtual = 16; // Tamanho inicial em pixels
+function diminuirFonte(){
 
-function mudarTamanhoFonte(mudanca) {
-    tamanhoAtual = tamanhoAtual + mudanca;
-    
-    // Limites para a fonte não ficar gigante ou minúscula demais
-    if (tamanhoAtual > 32) {
-        tamanhoAtual = 32;
-    } else if (tamanhoAtual < 12) {
-        tamanhoAtual = 12;
-    }
+tamanho-=2;
 
-    // Aplica o novo tamanho ao corpo do site
-    document.body.style.fontSize = tamanhoAtual + 'px';
+document.body.style.fontSize=tamanho+"px";
+
+}
+
+function contraste(){
+
+document.body.classList.toggle("altoContraste");
+
+}
+
+function lerTexto(){
+
+let texto=document.getElementById("conteudo").innerText;
+
+let fala=new SpeechSynthesisUtterance(texto);
+
+fala.lang="pt-BR";
+
+speechSynthesis.speak(fala);
+
+}
+
+function girarDisco(){
+
+document.getElementById("disco").classList.toggle("girar");
+
 }
